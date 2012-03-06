b1de58493cc472fa9dbe7d4cc7eac71c85df2ce2
eraserx99 <eraserx99@gmail.com>
2012-03-05 21:16:58 -0500

    more coming...

--- sample_app/spec/requests/layout_links_spec.rb
+++ sample_app/spec/requests/layout_links_spec.rb
@@ -21,4 +21,27 @@ describe "LayoutLinks" do
     get '/signup'
     response.should have_selector('title', :content => "Sign up")
   end
+  describe "when not signed in" do
+    it "should have a signin link" do
+      visit root_path
+      response.should have_selector("a", :href => signin_path, :content => "Sign in")
+    end
+  end
+  describe "when signed in" do
+    # The test_sign_in function doesn't work inside integration tests for some reason
+    before(:each) do
+      @user = Factory(:user)
+      visit signin_path
+      fill_in :email, :with => @user.email fill_in :password, :with => @user.password click_button
+    end
+    it "should have a signout link" do
+      visit root_path
+      response.should have_selector("a", :href => signin_path, :content => "Sign in")
+    end
+    it "should have a profile link" do
+      visit root_path
+      response.should have_selector("a", :href => user_path(@user), :content => "Profile")
+    end  
+  end
 end
+
